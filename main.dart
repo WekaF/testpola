c0298c42bcb18811c2e39abded348d5804b25896
Wahyu kurnia F <wahyukf01@gmail.com>
2022-02-11 00:17:04 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,22 @@
+
+import 'dart:io';
+
+void main() {
+
+int ip = 5;
+
+  for(int i= 1; i <= ip; i++){
+    for(int j= 1; j <= i; j++){
+    stdout.write('* ');
+    }
+    stdout.writeln(); 
+  }
+
+  for(int i = ip -1; i>=1; i--){
+  for(int j = 1; j<=i; j++){
+    stdout.write('* ');
+    }
+    stdout.writeln();
+  }
+ 
+}
